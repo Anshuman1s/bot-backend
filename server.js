260b2f33bd63acacaee9965ec68a6307f9cf6e78
Anshuman Shukla <anshumanshukla.12as@gmail.com>
2026-03-22 19:03:13 +0530

    App.listen

--- server.js
+++ server.js
@@ -17,7 +17,8 @@ app.get("/", (req, res) => {
 app.use("/chat", chatRoutes);
 const PORT = process.env.PORT || 5000;
 
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
 module.exports = app;
